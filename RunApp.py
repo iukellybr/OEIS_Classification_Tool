7052981d56faade121b182761da6631b979021c8
iukellybr <kellybr@iu.edu>
2025-04-17 02:32:28 -0400

    add a first pass at making this thing distributable!

--- RunApp.py
+++ RunApp.py
@@ -2,3 +2,6 @@ import subprocess
 
 # Run the Streamlit app
 subprocess.run(["python", "-m", "streamlit", "run", "OEIS_Classification_Tool/OEIS_Streamlit_App.py"])
+
+# Whenever wanting to package a new version of the app, run the following in your bash terminal - requires you to "pip install pyinstaller" first
+# python -m PyInstaller --noconfirm --onefile --console RunApp.py
