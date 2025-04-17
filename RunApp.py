2636f6d6500811e646f64102e40a0599d86189f8
iukellybr <kellybr@iu.edu>
2025-04-17 02:48:27 -0400

    slight cleanup to build command

--- RunApp.py
+++ RunApp.py
@@ -4,4 +4,4 @@ import subprocess
 subprocess.run(["python", "-m", "streamlit", "run", "OEIS_Classification_Tool/OEIS_Streamlit_App.py"])
 
 # Whenever wanting to package a new version of the app, run the following in your bash terminal - requires you to "pip install pyinstaller" first
-# python -m PyInstaller --noconfirm --onefile --console RunApp.py
+# python -m PyInstaller --noconfirm --onefile --clean --console --name OEIS_SequenceTool RunApp.py
